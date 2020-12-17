0f8c1c41e1f141496b939e200a67b5e0739710ee
peter <peter@2018-MBP.cogeco.local>
2020-12-17 17:26:20 -0500

    day 1 -- cleaned up a bit

--- aoc2020/src/day1.py
+++ aoc2020/src/day1.py
@@ -1,14 +1,16 @@
-import aoc2020
 from aoc2020.src import utils
 
 def part_a(nums):
+    print("part A")
     search_set = set(2020-x for x in nums)
     for num in nums:
         if num in search_set:
             print(num, "and", 2020-num, "are the numbers.")
             print('the answer:', num * (2020-num))
+            break
 
 def part_b(nums):
+    print("part B")
     search_set = dict()
     for x in nums:
         for y in nums:
@@ -17,8 +19,9 @@ def part_b(nums):
         if num in search_set.keys():
             print(num, ",", search_set[num][0],",",search_set[num][1], "are the numbers.")
             print('the answer:', num * search_set[num][0] * search_set[num][1])
+            break
 
 if __name__=='__main__':
     nums = utils.data(1)
-    #part_a(nums)
+    part_a(nums)
     part_b(nums)
